32928272a64b97a8257fee34781b34f13bd3699e
Miguel Henriquez <miguelisaiashenriquez>
2023-01-10 20:11:58 -0600

    add default component at launch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'personal-info', pathMatch: 'full' },
+  { path: 'personal-info', component: PersonalInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.css']
 })
-export class PersonalInfoComponent {
-
-}
+export class PersonalInfoComponent {}
